ce2458d0e270e7abc4fbb94430811971093ffe41
Your New Name <vipinkumarti555@gmail.com>
2024-12-17 21:42:58 +0530

    socket connection

---
+++ src/config/server.ts
@@ -0,0 +1,16 @@
+import { Socket,io } from "socket.io-client";
+
+let socket:Socket;
+
+export const getSocket =():Socket=>{
+    if (socket) {
+        return socket
+    }
+
+    socket = io(process.env.NEXT_PUBLIC_APP_URL as string , {
+        autoConnect:false
+    })
+
+    return socket
+
+}
