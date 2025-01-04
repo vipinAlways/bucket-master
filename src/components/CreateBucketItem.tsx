4384c2d02480bcca5c285e4fd940e34de3f662e4
Your New Name <vipinkumarti555@gmail.com>
2025-01-04 23:22:47 +0530

    changes

--- src/components/CreateBucketItem.tsx
+++ src/components/CreateBucketItem.tsx
@@ -1,176 +1,166 @@
-import { startTarget } from '@/app/actions/bucketList-action/bucketlist-action';
-import { useToast } from '@/hooks/use-toast';
-import { cn } from '@/lib/utils';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import React, { useState } from 'react'
-import { Button } from './ui/button';
+import { startTarget } from "@/app/actions/bucketList-action/bucketlist-action";
+import { useToast } from "@/hooks/use-toast";
+import { cn } from "@/lib/utils";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import React, { useState } from "react";
+import { Button } from "./ui/button";
 
 const CreateBucketItem = () => {
-      const [target, setTarget] = useState<TargetProps>({
-        duedate: new Date(),
-        type: "ShortMilestone",
-        budget: 0,
-        itemName: "",
+  const [target, setTarget] = useState<TargetProps>({
+    duedate: new Date(),
+    type: "ShortMilestone",
+    budget: 0,
+    itemName: "",
+  });
+  const [hidden, setHidden] = useState("hidden");
+  const [pending, setPending] = useState(false);
+  const { toast } = useToast();
+
+  const queryClient = useQueryClient();
+
+  const createBucketITem = useMutation({
+    mutationKey: ["create-bucket"],
+    mutationFn: startTarget,
+    onError: () =>
+      toast({
+        title: "Error",
+        description: "ServerError while creating bucket",
+        variant: "destructive",
+      }),
+    onSuccess: () => {
+      toast({
+        title: "Success",
+        description: "Bucket Created Successfully",
       });
-    const [hidden, setHidden] = useState("");
-    const [pending, setPending] = useState(false);
-    const {toast} = useToast()
+      setHidden("hidden");
+      queryClient.invalidateQueries({ queryKey: ["item-active"] });
+    },
+  });
 
-    const queryClient = useQueryClient();
-
-    const createBucketITem = useMutation({
-        mutationKey: ["create-bucket"],
-        mutationFn: startTarget,
-        onError: () =>
-          toast({
-            title: "Error",
-            description: "ServerError while creating bucket",
-            variant: "destructive",
-          }),
-        onSuccess: () => {
-          toast({
-            title: "Success",
-            description: "Bucket Created Successfully",
-          });
-          setHidden("hidden");
-          queryClient.invalidateQueries({ queryKey: ["item-active"] });
-        },
-        
-      });
-
-
-
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (createBucketITem.isPending) {
-          setPending(true)
-        }
-        createBucketITem.mutate({
-          duedate: target.duedate,
-          type: target.type,
-          budget: target.budget,
-          itemName: target.itemName,
-        });
-      };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (createBucketITem.isPending) {
+      setPending(true);
+    }
+    createBucketITem.mutate({
+      duedate: target.duedate,
+      type: target.type,
+      budget: target.budget,
+      itemName: target.itemName,
+    });
+  };
   return (
     <div className="w-full flex items-center justify-start max-h-80">
-    <div
-      className={cn(
-        "my-9 flex flex-col gap-4 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#f5edeb] p-4 rounded-lg h-full max-md:w-full max-md:h-3/5 w-[50%] z-40 px-10 py-9",
-        hidden
-      )}
-    >
-      <h1
-        className="absolute right-0 top-0 m-1 font-bold text-xl flex items-center justify-center  rounded-full border-2 border-zinc-600 text-orange-500 h-10 w-10 cursor-pointer"
-        onClick={() => setHidden("hidden")}
+      <div
+        className={cn(
+          "my-9 flex flex-col gap-4 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#f5edeb] p-4 rounded-lg h-full max-md:w-full max-md:h-3/5 w-[50%] z-40 px-10 py-9",
+          hidden
+        )}
       >
-        X
-      </h1>
-
-      <form
-        action=""
-        className="flex flex-col gap-4 w-full"
-        onSubmit={handleSubmit}
-      >
-        <input
-          type="text"
-          placeholder="Target Name"
-          className="w-full h-14 p-2 border border-[#ff3d13] rounded-lg"
-          value={target?.itemName}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            const targetName = e.target.value.toUpperCase() || "";
-            setTarget(
-              (prev) =>
-                ({
-                  ...prev,
-                  itemName: targetName,
-                } as TargetProps)
-            );
-          }}
-        />
-        <input
-          type="text"
-          placeholder="Budget Of Target"
-          className="w-full h-14 p-2 border border-[#ff3d13] rounded-lg"
-          value={target?.budget}
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            const newBudget = parseInt(e.target.value) || 0;
-            setTarget(
-              (prev) =>
-                ({
-                  ...prev,
-                  budget: newBudget,
-                } as TargetProps)
-            );
-          }}
-        />
-        <input
-          type="date"
-          placeholder="Due Date"
-          className="w-full h-14 p-2 border border-[#ff3d13] rounded-lg"
-          value={
-            target?.duedate
-              ? target.duedate.toISOString().split("T")[0]
-              : ""
-          }
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-            const dueDate = new Date(e.target.value);
-            setTarget(
-              (prev) =>
-                ({
-                  ...prev,
-                  duedate: dueDate,
-                } as TargetProps)
-            );
-          }}
-        />
-
-        <select
-          value={target.type}
-          onChange={(e) =>
-            setTarget((prev) => ({
-              ...prev,
-              type: e.target.value as TargetProps["type"],
-            }))
-          }
-          className="w-full h-14 p-2 border border-[#ff3d13] rounded-lg"
+        <h1
+          className="absolute right-0 top-0 m-1 font-bold text-xl flex items-center justify-center  rounded-full border-2 border-zinc-600 text-orange-500 h-10 w-10 cursor-pointer"
+          onClick={() => setHidden("hidden")}
         >
-          <option value="ShortMilestone">Short Milestone</option>
-          <option value="dreamMilestone">Dream Milestone</option>
-        </select>
+          X
+        </h1>
 
+        <form className="flex flex-col gap-4 w-full" onSubmit={handleSubmit} >
+          <input
+            type="text"
+            placeholder="Target Name"
+            className="w-full h-14 p-2 border border-[#ff3d13] rounded-lg"
+            value={target?.itemName}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              const targetName = e.target.value.toUpperCase() || "";
+              setTarget(
+                (prev) =>
+                  ({
+                    ...prev,
+                    itemName: targetName,
+                  } as TargetProps)
+              );
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Budget Of Target"
+            className="w-full h-14 p-2 border border-[#ff3d13] rounded-lg"
+            value={target?.budget}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              const newBudget = parseInt(e.target.value) || 0;
+              setTarget(
+                (prev) =>
+                  ({
+                    ...prev,
+                    budget: newBudget,
+                  } as TargetProps)
+              );
+            }}
+          />
+          <input
+            type="date"
+            placeholder="Due Date"
+            className="w-full h-14 p-2 border border-[#ff3d13] rounded-lg"
+            value={
+              target?.duedate ? target.duedate.toISOString().split("T")[0] : ""
+            }
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              const dueDate = new Date(e.target.value);
+              setTarget(
+                (prev) =>
+                  ({
+                    ...prev,
+                    duedate: dueDate,
+                  } as TargetProps)
+              );
+            }}
+          />
+
+          <select
+            value={target.type}
+            onChange={(e) =>
+              setTarget((prev) => ({
+                ...prev,
+                type: e.target.value as TargetProps["type"],
+              }))
+            }
+            className="w-full h-14 p-2 border border-[#ff3d13] rounded-lg"
+          >
+            <option value="ShortMilestone">Short Milestone</option>
+            <option value="dreamMilestone">Dream Milestone</option>
+          </select>
+
+          <Button
+            type="submit"
+            className="text-center text-2xl p-3 h-12 bg-[#ff3d13] hover:bg-[#ff3e13f2] text-[#f5edeb] w-40"
+            disabled={pending}
+          >
+            <span className="text-center flex items-center gap-4">
+              Add Target
+            </span>
+          </Button>
+        </form>
+      </div>
+      <div className="flex items-center justify-center flex-col gap-6">
         <Button
-          type="submit"
-          className="text-center text-2xl p-3 h-12 bg-[#ff3d13] hover:bg-[#ff3e13f2] text-[#f5edeb] w-40"
-          disabled={pending}
+          className="text-center text-4xl p-3 h-20 bg-[#ff3d13] hover:bg-[#ff3e13f2] text-[#f5edeb] w-96 md:hover-btn"
+          onClick={() => setHidden("")}
         >
           <span className="text-center flex items-center gap-4">
-            Add Target
+            Add to Bucket <span className="text-xl">&#128221; &#10024;</span>
           </span>
         </Button>
-      </form>
-    </div>
-    <div className="flex items-center justify-center flex-col gap-6">
-      <Button
-        className="text-center text-4xl p-3 h-20 bg-[#ff3d13] hover:bg-[#ff3e13f2] text-[#f5edeb] w-96 md:hover-btn"
-        onClick={() => setHidden("")}
-      >
-        <span className="text-center flex items-center gap-4">
-          Add to Bucket{" "}
-          <span className="text-xl">&#128221; &#10024;</span>
-        </span>
-      </Button>
-      <Button className="text-center text-4xl p-3 h-20 bg-[#ff3d13] hover:bg-[#ff3e13f2] text-[#f5edeb] w-80 md:hover-btn">
-        <span className="text-center flex items-center gap-4  ">
-          Track Record
-          <span className="text-xl">&#128221; &#10024;</span>
-        </span>
-      </Button>
-    </div>
+        <Button className="text-center text-4xl p-3 h-20 bg-[#ff3d13] hover:bg-[#ff3e13f2] text-[#f5edeb] w-80 md:hover-btn">
+          <span className="text-center flex items-center gap-4  ">
+            Track Record
+            <span className="text-xl">&#128221; &#10024;</span>
+          </span>
+        </Button>
+      </div>
 
-    <div></div>
-  </div>
-  )
-}
+      <div></div>
+    </div>
+  );
+};
 
-export default CreateBucketItem
+export default CreateBucketItem;
--- src/components/TimeCountDown.tsx
+++ src/components/TimeCountDown.tsx
@@ -1,13 +1,11 @@
 "use client";
-
 import { activeBucketItem } from "@/app/actions/bucketList-action/bucketlist-action";
 import { cn } from "@/lib/utils";
 import { useQuery } from "@tanstack/react-query";
 import React, { useEffect, useState } from "react";
 
 const TimeCountDown = () => {
-  const [time, setTime] = useState<Date | null>(new Date());
-  const [onHold, setOnHold] = useState(false);
+  const [time, setTime] = useState<Date | undefined | null>(new Date());
 
   const [remainingTime, setRemainingTime] = useState({
     days: 0,
@@ -17,26 +15,20 @@ const TimeCountDown = () => {
   });
 
   const { data } = useQuery({
-    queryKey: ["active-time"],
+    queryKey: ["item-active"],
     queryFn: async () => activeBucketItem(),
   });
 
   useEffect(() => {
-    if (data) {
-      if (data.onHold) {
-        setOnHold(true);
-        setTime(null);
-        setRemainingTime({ days: 0, hours: 0, minutes: 0, seconds: 0 });
-      } else if (data.duedate) {
-        setOnHold(false);
-        setTime(new Date(data.duedate));
-      }
+    if (data?.duedate) {
+      setTime(new Date(data.duedate));
     }
+
+    console.log(data, "data");
   }, [data]);
 
   useEffect(() => {
-    if (!time || onHold) return;
-
+    if (!time) return;
     const updateRemainingTime = () => {
       const now = new Date();
       const diff = time.getTime() - now.getTime();
@@ -45,6 +37,9 @@ const TimeCountDown = () => {
         setRemainingTime({ days: 0, hours: 0, minutes: 0, seconds: 0 });
         return;
       }
+
+      if (data?.onHold)
+        setRemainingTime({ days: 0, hours: 0, minutes: 0, seconds: 0 });
 
       const days = Math.floor(diff / (1000 * 60 * 60 * 24));
       const hours = Math.floor(
@@ -60,10 +55,14 @@ const TimeCountDown = () => {
     updateRemainingTime();
 
     return () => clearInterval(interval);
-  }, [time, onHold]);
+  }, [time, data?.onHold,data?.Active]);
 
   return (
-    <div className="flex flex-col w-full font-bucket md:text-6xl text-4xl items-center text-center selection:select-text text-transparent-border">
+    <div
+      className={`flex flex-col w-full  font-bucket md:text-6xl text-4xl items-center text-center selection:select-text  text-transparent-border  ${
+        !data?.Active && "hidden"
+      }`}
+    >
       <div
         className={cn(
           "grid grid-cols-4 max-md:gap-2 w-full items-center",
@@ -71,15 +70,15 @@ const TimeCountDown = () => {
         )}
       >
         <p className="text-center w-full">{remainingTime.days}</p>
-        <p className="text-center w-full">{remainingTime.hours}</p>
-        <p className="text-center w-full">{remainingTime.minutes}</p>
-        <p className="text-center w-full">{remainingTime.seconds}</p>
+        <p className="text-center w-full"> {remainingTime.hours}</p>
+        <p className="text-center w-full"> {remainingTime.minutes}</p>
+        <p className="text-center w-full"> {remainingTime.seconds}</p>
       </div>
-      <div className="grid grid-cols-4 w-full md:text-2xl text-xl max-md:gap-2">
-        <p className="text-center">Days</p>
-        <p className="text-center">Hrs</p>
-        <p className="text-center">Min</p>
-        <p className="text-center">Sec</p>
+      <div className="grid grid-cols-4 ic w-full md:text-2xl text-xl max-md:gap-2">
+        <p className="text-center ">Days</p>
+        <p className="text-center ">Hrs</p>
+        <p className="text-center ">Min</p>
+        <p className="text-center ">Sec</p>
       </div>
     </div>
   );
