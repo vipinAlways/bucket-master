83fc23466fa33ff6e55188858a866cc43eb96d6e
Your New Name <vipinkumarti555@gmail.com>
2025-06-27 14:37:28 +0530

    started with llm now

--- src/app/api/get-roadmap/route.ts
+++ src/app/api/get-roadmap/route.ts
@@ -1,11 +1,38 @@
-import { anthropic } from "@/Openai";
+// app/api/ask/route.ts
+"use server";
+
+import OpenAI from "openai";
 import { NextResponse } from "next/server";
 
+
+const client = new OpenAI({
+  baseURL: "https://api.sambanova.ai/v1", 
+  apiKey: process.env.SAMBANOVA_API_KEY,  
+});
+
 export async function POST(req: Request) {
-  const msg = await anthropic.messages.create({
-    model: "claude-opus-4-20250514",
-    max_tokens: 1024,
-    messages: [{ role: "user", content: "Hello, Claude" }],
-  });
-  console.log(msg);
+  try {
+    const prompt  = "hii there";
+
+    const chatCompletion = await client.chat.completions.create({
+      messages: [
+        { role: "system", content: "Answer the question in a couple sentences." },
+        { role: "user", content: prompt },
+      ],
+      model: "Meta-Llama-3.1-8B-Instruct",
+      max_tokens: 300,
+      temperature: 0.7,
+    });
+
+
+
+    const answer = chatCompletion && chatCompletion.choices[0].message.content;
+    return NextResponse.json({ response: answer ,chatCompletion});
+
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.message || "Something went wrong" },
+      { status: 500 }
+    );
+  }
 }
